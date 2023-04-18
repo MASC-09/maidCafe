b8a37bfa647751f458538e88860ff98718499d2b
Miguel Soto <miguelsoto@Miguels-MacBook-Pro.local>
2023-04-18 10:06:19 -0600

    Fixed NPC animations based on behaviors, add all the targets for the NPCs to seat

--- Assets/Scripts/NavMeshController.cs
+++ Assets/Scripts/NavMeshController.cs
@@ -10,10 +10,16 @@ public class NavMeshController : MonoBehaviour
     public float movementSpeed;
     private enum animationState {idle, walking, sit_idle, sit_pointing, sit_angry};
     public Animator anim;
-    private bool isAtSeat;
-    private bool isMoving; //this could be replaced for just a value greater than 0 on the movement vector
-    public bool hasOrdered = false;
-    public bool fewTime;
+    [SerializeField] private bool isAtSeat = false;
+    [SerializeField] private bool isMoving; //this could be replaced for just a value greater than 0 on the movement vector
+    [SerializeField] public bool hasOrdered = false;
+    [SerializeField] public bool hasEaten = false;
+    [SerializeField] public bool isAlive = true;
+    [SerializeField] public float destructionDelay = 40.0f;
+    [SerializeField] public float timer = 0.0f;
+    [SerializeField] public bool timerStarted = false;
+    
+
 
 
 
@@ -30,13 +36,25 @@ public class NavMeshController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.destination = target.position;
-        isMoving = agent.velocity.magnitude > 0.1f; //Check if agent is moving
+
+         if (timerStarted)
+        {
+            timer += Time.deltaTime;
+        }
+        
+        if (timer >= destructionDelay)
+        {
+            killNPC();
+        }
+        if(!isAtSeat)
+        {
+            agent.destination = target.position;
+            isMoving = agent.velocity.magnitude > 0.1f; //Check if agent is moving
+        }
         UpdateAnimationState();
-        // Check_ReachSeat();
-
-
+        
     }
+
 
     private void UpdateAnimationState()
     {
@@ -47,25 +65,34 @@ public class NavMeshController : MonoBehaviour
         {
             state = animationState.walking;
         }
+
         if (isAtSeat)
         {
-            state = animationState.sit_idle;
-            // if (!hasOrdered)    
-            // {
-            //     state = animationState.sit_pointing;
-            // }
-            // else if (fewTime)
-            // {
-            //     state = animationState.sit_idle;
-            // }
-            // else 
-            // {
-            //     state = animationState.sit_angry;
-            // }
+            if (hasOrdered)
+            {
+                if (timer > 30f)
+                {
+                    state = animationState.sit_angry;
+                    Debug.Log("Animation: sit_angry");                 
+                }
+                state = animationState.sit_idle;
+                Debug.Log("Animation: sit_idle");                 
+            }
+            else
+            {
+                if (timer > 10f)
+                {
+                    state = animationState.sit_angry;
+                    Debug.Log("Animation: sit_angry");
+                }
+                else
+                {
+                    state = animationState.sit_pointing;
+                    Debug.Log("Animation: sit_poiting");    
+                }
+            }
         }
         anim.SetInteger("state",(int)state);
-        // Debug.Log("current state " + state);
-
     }
 
     private void OnTriggerEnter(Collider other)
@@ -73,10 +100,44 @@ public class NavMeshController : MonoBehaviour
         if (other.CompareTag("seat"))
         {
             isAtSeat = true;
-            Debug.Log("reached seat");
-            anim.SetInteger("state",(int)animationState.sit_idle);        
+            // StartCoroutine(logger("reached seat, starting counter"));
+            timerStarted = true;                      
+            Debug.Log("Reached seat, starting timer.");                 
+
+        }
+        if (other.CompareTag("Test"))
+        {
+            isAtSeat = true;
+            // StartCoroutine(logger("reached seat, starting counter"));
+            timerStarted = true;     
+            Debug.Log("Collided with Test.");                 
+
         }
     }
 
+    //function that destroys the game object after the time is completed.
+    // IEnumerator DestroyAfterDelay()
+    // {
+    //     yield return new WaitForSeconds(destructionDelay);
+    //     killNPC();
+    // }
+    private void clientServed()
+    {
+        killNPC();
+    }
+
+    private void killNPC()
+    {
+        Destroy(gameObject);
+        // StartCoroutine(logger("NPC destroyed"));
+    }
+
+    // IEnumerator logger(string log)
+    // {
+    //     yield return new WaitForSeconds(5.0f);
+    //     Debug.Log(log);
+    // }
     
+
+
 }
